45e99489c5e4532cdf0cc7d620dffb72195200f0
Maxim Aleynikov <max.aleyinikov@gmail.com>
2025-05-14 15:49:36 -0400

    main: add login form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 import { useTranslation } from 'react-i18next';
 import Auth from './servises/Auth';
+import Button from '@mui/material/Button';
+import { Box, TextField, Typography } from '@mui/material';
 
 function App() {
   const { t } = useTranslation();
@@ -14,17 +16,49 @@ function App() {
   }
 
   return (
-    <div id="App">
-      <h1 className="title">{t('welcome')}</h1>
-      <div className="prompt">{t('prompt_name')}</div>
-      <div id="login" className="input-box">
-        <input id="name"
-          className="input"
-          autoComplete="off"
-          name="input"
-          type="text"
-        />
-        <button className="btn" onClick={login}>{t('login')}</button>
+    <div id="app">
+      <div id="login-box">
+        <Typography variant="h4" component="div" sx={{
+          textAlign: 'center',
+          marginBottom: 6,
+          fontWeight: 'bold'
+        }}>
+          {t('welcome')}
+        </Typography>
+        <Typography component="p" sx={{
+          textAlign: 'center',
+          marginBottom: 2,
+        }}>
+          {t('prompt_name')}
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 2,
+          }}
+        >
+          <TextField
+            id="name"
+            name="name"
+            label={t('your_name')}
+            size="small"
+            variant="outlined"
+            sx={{ width: '200px' }}
+            autoFocus
+            required
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={login}
+            sx={{ marginLeft: 2 }}
+          >
+            {t('login')}
+          </Button>
+        </Box>
       </div>
     </div>
   )
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,15 @@ import './style.css'
 import App from './App'
 import { BrowserRouter, Routes, Route } from 'react-router';
 import './i18n';
+import { CssBaseline } from '@mui/material';
+
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
 
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
