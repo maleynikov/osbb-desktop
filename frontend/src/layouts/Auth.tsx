18c4ff052bc24c775074058035c4e62fc25622a7
Maxim Aleynikov <max.aleyinikov@gmail.com>
2025-05-15 15:22:13 -0400

    main: add formik

--- frontend/src/layouts/Auth.tsx
+++ frontend/src/layouts/Auth.tsx
@@ -4,22 +4,24 @@ import { Outlet } from "react-router";
 
 const Copyright = (props: any) => {
   return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {'Copyright © '}
-      <Link color="inherit" href="https://github.com/maleynikov/osbb-desktop/">
-        OSBB. Desctop App
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-      <Typography variant="body2" sx={{ mt: 1 }}>
+    <>
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="center"
+        {...props}
+      >
+        {'Copyright © '}
+        <Link color="inherit" href="https://github.com/maleynikov/osbb-desktop/">
+          OSBB. Desctop App
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+      <Typography variant="body2" align="center" sx={{ mt: 1 }}>
         💖 <Link color="inherit" href="https://wails.io/">Wails</Link>
       </Typography>
-    </Typography>
+    </>
   );
 };
 
--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
 import { useTranslation } from 'react-i18next';
 import './styles.css';
+import { Formik } from 'formik';
 
 
 const LoginPage = () => {
@@ -24,35 +25,61 @@ const LoginPage = () => {
       }}>
         {t('prompt_name')}
       </Typography>
-      <Box
-        component="form"
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginTop: 2,
+      <Formik
+        initialValues={{ username: ''}}
+        validate={(values) => {
+          const errors: any = {};
+          if (!values.username) {
+            errors.username = t('required');
+          }
+          return errors;
         }}
+        onSubmit={handleSubmit}
       >
-        <TextField
-          id="name"
-          name="name"
-          label={t('your_name')}
-          size="small"
-          variant="outlined"
-          sx={{ width: '200px' }}
-          autoFocus
-          autoComplete="off"
-          required
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleSubmit}
-          sx={{ marginLeft: 2 }}
-        >
-          {t('login')}
-        </Button>
-      </Box>
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 2,
+            }}
+          >
+            <TextField
+              name="username"
+              label={t('your_name')}
+              size="small"
+              variant="outlined"
+              sx={{ width: '200px' }}
+              autoComplete="off"
+              error={Boolean(errors.username)}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.username}
+              autoFocus
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              disabled={isSubmitting}
+              sx={{ marginLeft: 2 }}
+            >
+              {t('login')}
+            </Button>
+        </Box>
+        )}
+      </Formik>
   </div>
   );
 };
